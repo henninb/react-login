235d5d4cda16aa9c3890109fc05dd9864e1039cb
Brian Henning <henninb@msn.com>
2022-04-09 13:30:17 -0500

    initial version

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo npx create-react-app react-app
+pnpm install
+pnpm start
+
+exit 0
---
+++ src/components/Login.jsx
@@ -0,0 +1,41 @@
+import react from 'react';
+import axios from 'axios';
+import './style.css';
+
+// export default const LoginPage = (props) => {
+export default function LoginPage(props) {
+
+    return (
+      <body>
+
+       <h1>header1</h1>
+
+        <div className="login">
+            <div className="form">
+                <form name="login-form" className="login-form" action="/login" method="GET" data-bitwarden-watching="1">
+                    <span className="material-icons">lock</span>
+                    <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                 <span className="input-group-text">
+                  <i className="fa fa-user form-icon"></i>
+                 </span>
+                </div>
+                <input type="text" className="form-control" placeholder="Email" id="email" name="email" />
+             </div>
+             <div className="input-group mb-3">
+               <div className="input-group-prepend">
+                 <span className="input-group-text">
+                   <i className="fa fa-lock form-icon"></i>
+                 </span>
+               </div>
+               <input type="password" className="form-control" placeholder="Password" id="password" name="password" />
+             </div>
+                    <button type="submit">login</button>
+                </form>
+            </div>
+        </div>
+
+
+       </body>
+    );
+};
