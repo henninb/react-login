771e84f672a3f0750e5f65c0d4e5a2eae9a06951
Brian Henning <henninb@msn.com>
2022-04-10 07:36:40 -0500

    wip

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const axios = require('axios')
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3001
+const app = express()
+
+app.listen(port, () => { console.log(`listening on port ${port}`) });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/example-react/build`));
+// app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/api/login', (req, res) => {
+  // res.json({username: "example"});
+   res.send('put the schedule here');
+});
+
--- src/components/HockeyScores.jsx
+++ src/components/HockeyScores.jsx
@@ -5,7 +5,8 @@ import data from '../data/wild-schedule.json';
 
 export default function HockeyScores() {
 
-  const [schedule, setSchedule] = useState('');
+  const [schedule, setSchedule] = useState('')
+  const [login, setLogin] = useState('')
 
   // var config = {
   //   method: 'get',
@@ -14,38 +15,45 @@ export default function HockeyScores() {
   //   }
   // };
 
+  async function apiCall() {
+      const response = await axios.get("/api/login")
+      return response.data;
+    }
 
-  const config = {
-  method: 'GET',
-  url: 'https://api-nba-v1.p.rapidapi.com/teams',
-  headers: {
-    'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': '632241f898msh5c971d6074fb435p1518cajsn2db7c8d6f421'
-  }
-};
 
-  function showSchedule() {
+  // const config = {
+  // method: 'GET',
+  // url: 'https://api-nba-v1.p.rapidapi.com/teams',
+  // headers: {
+  //   'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
+  //   'X-RapidAPI-Key': '632241f898msh5c971d6074fb435p1518cajsn2db7c8d6f421'
+  // }
+// }
+
+  async function showSchedule(e) {
     const newdata = data.map( (data) => {
       if( data.HomeTeamScore === null ) {
         if( data.HomeTeam === 'Minnesota Wild' ) {
-           console.log(data.DateUtc + " - vs " + data.AwayTeam);
+           console.log(data.DateUtc + " - vs " + data.AwayTeam)
         }
         if( data.AwayTeam === 'Minnesota Wild' ) {
-          console.log(data.DateUtc + " - at " + data.HomeTeam);
+          console.log(data.DateUtc + " - at " + data.HomeTeam)
         }
       }
-    });
-  };
+    })
+
+    setLogin(await apiCall())
+  }
 
 
   // useEffect(async () => {
-  // }, []);
+  // }, [])
 
     return (
       <div>
        <h1>Hockey Scores</h1>
       <button onClick={showSchedule}>Show Schedule</button>
-      <div id="content"></div>
+      <div id="content">{login}</div>
       </div>
-    );
-};
+    )
+}
