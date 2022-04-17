ba78f5b2f85b43829e09289abdb180cf00ba17a2
Brian Henning <henninb@msn.com>
2022-04-17 07:46:08 -0500

    wip

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -5,17 +5,24 @@ export default function Temperature() {
   async function postCall(event) {
      console.log(event.formData);
      event.preventDefault()
-     //const form = document.getElementById("temperature-input")
-     //const formEntries = new FormData(form).entries();
-     //console.log(formEntries)
-     console.log(event.formData);
-     const formData = new FormData(event.target);
-     console.log("f=" + formData.get('fahrenheit'));
+     // const form = document.getElementById("temperature-input")
+     // const formEntries = new FormData(form).entries();
+     // console.log(formEntries)
+     // console.log(event.formData);
+     // console.log(event.target);
+
+     let fahrenheit = document.getElementById("fahrenheit").value;
+     console.log(fahrenheit);
+     let data = {
+        fahrenheit: fahrenheit,
+     };
+     // const formData = new FormData(event.target);
+     // console.log("f=" + formData.get('fahrenheit'));
   // Now you can use formData.get('foo'), for example.
 
-     const body = '{"fahrenheit":33.0}'
+     // const body = '{"fahrenheit":33.0}'
        try {
-        const response = await axios.post("/celsius", body,
+        const response = await axios.post("/celsius", data,
         {
           headers: { 'Content-Type': 'application/json', }
         }
@@ -37,7 +44,7 @@ export default function Temperature() {
     return (
         <div>
            <form name="temperature-input">
-           <input type="text" name="fahrenheit" />
+           <input type="text" name="fahrenheit" id="fahrenheit" />
            <button onClick={postCall}>test</button>
            </form>
         </div>
