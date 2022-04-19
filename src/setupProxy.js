1642431548e08ac7b54b0671bd3531c64f222a0c
Brian Henning <henninb@msn.com>
2022-04-19 06:05:13 -0500

    wip

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // app.use(
-  //   "/api_local",
-  //   createProxyMiddleware({
-  //     target: "http://localhost:3080",
-  //     changeOrigin: true,
-  //   })
-  // );
+
   app.use(
     "/feed/json/nhl-2021/minnesota-wild",
     createProxyMiddleware({
       target:
         "https://fixturedownload.com",
-        // "https://fixturedownload.com/feed/json/nhl-2021/minnesota-wild",
-        //"https://fixturedownload.com/feed/json/nhl-2021/minnesota-wild",
-        //"https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false",
       // secure: true,
       // loglevel: 'debug',
       headers: {
@@ -29,6 +20,23 @@ module.exports = function (app) {
     })
   );
 
+  app.use(
+    "/feed/json/nba-2021/minnesota-timberwolves",
+    createProxyMiddleware({
+      target:
+        "https://fixturedownload.com",
+      // secure: true,
+      // loglevel: 'debug',
+      headers: {
+        accept: "application/json",
+        method: "GET",
+      },
+      changeOrigin: true,
+      // router: {
+      //   "/api/admin": "https://culture.seocho.go.kr:3000",
+      // },
+    })
+  );
   // app.use(
   //   "/api_coin",
   //   createProxyMiddleware({
