70f01e99e744523773b4e9467d9fc4eec1936362
Brian Henning <henninb@msn.com>
2022-04-20 06:07:25 -0500

    wip

--- src/components/BaseballScores.jsx
+++ src/components/BaseballScores.jsx
@@ -9,19 +9,27 @@ export default function BaseballScores() {
   const fetchBaseballSchedule = useCallback(async () => {
 
     const params = {
-  startDate: "1/01/2022",
-  endDate: "12/31/2022",
+      startDate: "1/01/2022",
+      endDate: "12/31/2022",
       gameTypes: "R",
       sportId: 1,
       teamId: 142,
       hydrate:"decisions"
+    };
 
-};
        try {
         const response = await axios.get("/api/v1/schedule", {params})
         console.log('apiCall was made.');
-         console.log(response.data);
-         setData(response.data);
+         console.log(response.data.dates);
+
+         Object.entries(response.data.dates).forEach((entry) => {
+           const [key, value] = entry;
+           // console.log(`${key}: ${JSON.stringify(value)}`);
+           console.log(value.date);
+           console.log(value.games);
+         });
+
+         setData(response.data.dates);
        } catch(error) {
          if(error) {
            console.log(error.data);
